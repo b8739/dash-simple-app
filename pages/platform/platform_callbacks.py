8e9ee9e1d08092297a5c961b9749bcb41fd970c4
정재호 <b8739@hanmail.net>
2022-04-13 22:10:37 +0900

    Completed: Proc_rate_A, Proc_rate_B

--- pages/platform/platform_callbacks.py
+++ pages/platform/platform_callbacks.py
@@ -10,6 +10,8 @@ import sys
 
 sys.path.append("./pages/platform")
 
+""" GET DATAFRAME """
+
 
 @application.callback(
     ServersideOutput("first_bio_store", "data"),
@@ -31,6 +33,9 @@ def save_first_data(n_clicks):
 def save_second_data(n_clicks):
     df = pd.read_excel("pages/platform/ketep_biogas_data_20220411_02.xlsx")
     return df
+
+
+""" DRAW GRAPH 1 """
 
 
 @application.callback(
@@ -79,4 +84,76 @@ def update_biggraph(first_df, second_df):
     return fig
 
 
+""" DRAW GRAPH 2 """
+
+
+@application.callback(
+    Output("proc_rate_a", "figure"),
+    Input("first_bio_store", "data"),
+)
+@cache.memoize(timeout=TIMEOUT)
+def update_rate_graph(df):
+    fig = px.line(df, x="Date", y="Proc_rate_A", markers=True)
+    fig.update_traces(
+        mode="markers+lines",
+        marker=dict(size=1, line=dict(width=0.5, color="#f4d44d")),
+        line=dict(color="#f4d44d", width=0.5),
+    ),
+    fig.update_layout(template="plotly_dark")
+    fig.update_layout(
+        yaxis_title=None,
+        xaxis_title="Date",
+        # updatemenus와 곂치기 때문에 none
+        title={
+            "text": "Proc_rate_A",
+            "xref": "paper",
+            "yref": "paper",
+            "x": 0.5,
+            # "pad": {"b": 50},
+            "xanchor": "center",
+            "yanchor": "middle",
+            "font": {"size": 15},
+            # "y": 0.5,
+        },
+        margin=dict(l=35, r=35, t=50, b=50, pad=20),
+        # pad=dict(l=100, r=100, t=30, b=100),
+    )
+    return fig
+
+
+@application.callback(
+    Output("proc_rate_b", "figure"),
+    Input("second_bio_store", "data"),
+)
+@cache.memoize(timeout=TIMEOUT)
+def update_rate_graph(df):
+    fig = px.line(df, x="Date", y="Proc_rate_B", markers=True)
+    fig.update_traces(
+        mode="markers+lines",
+        marker=dict(size=1, line=dict(width=0.5, color="#f4d44d")),
+        line=dict(color="#f4d44d", width=0.5),
+    ),
+    fig.update_layout(template="plotly_dark")
+    fig.update_layout(
+        yaxis_title=None,
+        xaxis_title="Date",
+        # updatemenus와 곂치기 때문에 none
+        title={
+            "text": "Proc_rate_B",
+            "xref": "paper",
+            "yref": "paper",
+            "x": 0.5,
+            # "pad": {"b": 50},
+            "xanchor": "center",
+            "yanchor": "middle",
+            "font": {"size": 15},
+            # "y": 0.5,
+        },
+        margin=dict(l=35, r=35, t=50, b=50, pad=20),
+        # pad=dict(l=100, r=100, t=30, b=100),
+    )
+    return fig
+
+
+# Proc_rate_A
 # Output({"type": "indicator", "index": MATCH}, "color"),
